5b6cffe273540b7458c66d0c533fd01d00afee84
acboyty <taoyangah@foxmail.com>
2020-10-16 14:59:56 +0800

    improve

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ class config:
     method = 'improve'
 
     # 'heart', 'cleveland', 'dermatology', 'ionosphere', 'wine', 'vehicle', 'sonar', 'glass', 'segmentation', 'hepatitis'
-    data = 'sonar'
+    data = 'heart'
 
     # 1NN, 3NN, 5NN, SVM, C4.5
-    classifier = '5NN'
+    classifier = '3NN'
 
     life_time = 15
     area_limit = 50
     transfer_rate = 0.05
 
     # 10-fold, 2-fold, 7-3
-    fitness_type = '7-3'
+    fitness_type = '10-fold'
 
     # usually 10-50
     epoch = 15
